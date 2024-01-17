ecdf547b685ca8fc4f9e326161ccd48e87b6961f
renatoppuccini <renatoppuccini@gmail.com>
2024-01-17 14:57:19 -0500

    Update main.py
    rest api url updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 # Set URL
 # Run: 
 # update REST_API_URL to your backend link (currently not dynamic)
-REST_API_URL = "https://inference-rosa-workshop-test.apps.cluster-wrfsc.wrfsc.gcp.redhatworkshops.io/api/predict"
+REST_API_URL = "https://inference-rosa-workshop-git-app-inference.apps.kg84l7w3.eastus.aroapp.io/api/predict"
 
 # Inference!
 def inference(image_path):
